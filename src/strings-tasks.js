42a4e7e702d69165c9eb6bbdecc5ab180a6b6008
ankkiel <anastasia_ermolova@bk.ru>
2023-11-14 16:01:18 +0300

    feat: add solutions for string comparison tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -204,8 +204,8 @@ function sumOfCodes(/* str */) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -219,8 +219,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -281,8 +281,8 @@ function orderAlphabetically(str) {
  *   containsSubstring('JavaScript is Fun', 'Python') => false
  *   containsSubstring('12345', '34') => true
  */
-function containsSubstring(/* str, substring */) {
-  throw new Error('Not implemented');
+function containsSubstring(str, substring) {
+  return str.includes(substring);
 }
 
 /**
@@ -316,8 +316,14 @@ function countVowels(/* str */) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let modifiedStr = str.toLowerCase().replace(/[!?,]/g, '');
+
+  if (modifiedStr.split(' ').length > 1) {
+    modifiedStr = modifiedStr.split(' ').join('');
+  }
+
+  return modifiedStr === modifiedStr.split('').reverse().join('');
 }
 
 /**
